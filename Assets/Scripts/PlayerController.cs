74025f69a5bda1d0f7cb3410b9879132078b9d4f
zinine73 <zinine@naver.com>
2024-10-15 15:36:55 +0900

    Prototype 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15;
+    private float speed = 15;
+    private float turnSpeed = 20;
+    private float horizontalInput;
+    private float forwardInput;
+
     void Update()
     {
-        // 차량을 앞으로 이동시킨다
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Input Manager를 통해 차량을 이동, 가속시킨다
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // forwardInput 값에 따라 차량을 앞으로, 뒤로 이동시킨다
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // horizontalInput 값에 따라 차량을 회전시킨다
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
