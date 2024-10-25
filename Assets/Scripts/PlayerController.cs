a12d2bdac7edaafcea3be9c7d5f23de2964fbf42
zinine73 <zinine@naver.com>
2024-10-25 15:50:34 +0900

    p1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 15;
-    private float turnSpeed = 20;
-    private float horizontalInput;
-    private float forwardInput;
+    [SerializeField] private float horsePower = 50;
+    [SerializeField] private float turnSpeed = 20;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float forwardInput;
+    [SerializeField] GameObject centerOfMass;
+    private Rigidbody rb;
 
-    void Update()
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    void FixedUpdate()
     {
         // Input Manager를 통해 차량을 이동, 가속시킨다
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         // forwardInput 값에 따라 차량을 앞으로, 뒤로 이동시킨다
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        rb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
         // horizontalInput 값에 따라 차량을 회전시킨다
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
